b5400bec92be12692065fa4afe68fa8e5ea562b3
zechariahjose <21103530@usc.edu.ph>
2025-12-25 16:55:27 +0800

    more notes Exam3

---
+++ Exam3/listToMaxHeap.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+
+typedef struct {
+    int data[MAX];
+    int lastNdx;
+} HeapList;
+
+void listToHeap(HeapList *H) {
+    int i = (H->lastNdx - 1) / 2;
+
+    for(; i >= 0; i--) {
+        int parent = i;
+        int largest = parent;
+
+        while(largest == parent) {
+            int Lchild = parent * 2 + 1;
+            int Rchild = Lchild + 1;
+
+            if(Lchild <= H->lastNdx && H->data[largest] < H->data[Lchild]) {
+                largest = Lchild;
+            }
+
+            if(Rchild <= H->lastNdx && H->data[largest] < H->data[Rchild]) {
+                largest = Rchild;
+            }
+
+            if(largest != parent) {
+                int temp = H->data[parent];
+                H->data[parent] = H->data[largest];
+                H->data[largest] = temp;
+                parent = largest;
+            }
+            else {
+                largest = -1; // flag
+            }
+        }
+    }
+}
+
+int main() {
+    return 0;
+}
+
